7fcf938cba0a2d57461beb628a5860ddebaf1f94
Scott Horton <horton.rscott@gmail.com>
2023-01-29 16:44:40 -0600

    Add launch file to only launch rviz

---
+++ launch/rviz_launch.py
@@ -0,0 +1,47 @@
+# Portions of this file are from the Elsabot project by Scott Horton
+# and others from LinoRobot2
+
+# Copyright (c) 2021 Juan Miguel Jimeno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('elsabot_jeep'), 'rviz', 'navigation.rviz']
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='sim', 
+            default_value='false',
+            description='Enable use_sime_time to true'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            parameters=[{'use_sim_time': LaunchConfiguration("sim")}]
+        )
+    ])
